afd77e0244d4d04b9c9a99c174dacabc342f5188
vercettiprada <xaviersaligumba@gmail.com>
2025-04-08 23:05:52 +0800

    changes in user and web php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,53 +21,53 @@ class UserController extends Controller
 
     public function getUsers()
     {
+        // Retrieve all users from the database using the connection to MySQL.
         $users = DB::connection('mysql')->select("SELECT * FROM tbl_user");
         return $this->successResponse($users);
     }
 
     public function index()
     {
+        // Retrieve all users using the Eloquent model.
         $users = User::all();
         return $this->successResponse($users);
     }
 
     public function add(Request $request)
     {
+        // Validation rules for adding a new user.
         $rules = [
             'username' => 'required|max:20',
             'password' => 'required|max:20',
             'gender' => 'required|in:Male,Female',
         ];
 
+        // Validate the incoming request.
         $this->validate($request, $rules);
 
+        // Create a new user in the database.
         $user = User::create($request->all());
 
-
+        // Return the created user with a 201 status.
         return $this->successResponse($user, Response::HTTP_CREATED);
     }
 
     public function show($id)
     {
-    $user = User::findOrFail($id);
-    return $this->successResponse($user); 
-       /*
-        $user = User::where('id', $id)->first();
-
-        if ($user) {
-            return $this->successResponse($user);
-        } else {
-            return $this->errorResponse('User Id Does Not Exist', Response::HTTP_NOT_FOUND);
-        }
-        */
+        // Attempt to find the user or fail with a 404.
+        $user = User::findOrFail($id);
+        return $this->successResponse($user); 
     }
-public function delete($id)
-{
 
-    $user = User::findOrFail($id);
-    $user->delete();
-    return $this->errorResponse('User ID Does Not Exist', Response::HTTP_NOT_FOUND);
-    
+    public function delete($id)
+    {
+        // Attempt to find the user by ID.
+        $user = User::findOrFail($id);
+        
+        // Delete the user.
+        $user->delete();
 
+        // Return success message after deletion.
+        return $this->successResponse('User successfully deleted', Response::HTTP_OK);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@
 |--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register all of the routes for an application.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
@@ -15,10 +14,6 @@
 
 $router->get('/', function () use ($router) {
     return $router->app->version();
-});
-
-$router->group(['prefix' => 'api'], function ()use ($router){
-    $router->get('/users',['uses' => 'UserController@getUsers']);
 });
 
 // Grouping API routes
